9a4060fa448ca404c28b227fed7ad8e2d154bd78
AKnot47 <arikev2007@gmail.com>
2023-05-24 10:36:18 -0700

    login and signup

--- app.py
+++ app.py
@@ -1,10 +1,85 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session
+from sqlalchemy import text
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from database import login_required, get_user, insert_user
 
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+#-------------
+#LOGIN-REQUIRED
 @app.route("/")
+@login_required
 def index():
-    return "hello world"
+   return render_template("home.html")
+
+
+#----------------
+#LOGIN
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if not request.form.get("username"):
+            return ("must provide username")
+        
+        elif not request.form.get("password"):
+            return ("must provide password")
+        
+        user = get_user(username)
+        if not user:
+            return "invalid user"
+        elif not check_password_hash(user[0]["password_hash"], password):
+            return "invalid password"
+        session["user_id"] = user[0]["id"]
+        return redirect("/")  
+    
+    else:
+        return render_template("login.html")
+
+#--------------------
+#SIGNUP
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        email = request.form.get("email")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm = request.form.get("confirm")
+
+        if not request.form.get("username"):
+            return ("must provide username")
+        
+        elif not request.form.get("password"):
+            return ("must provide password")
+        
+        elif not request.form.get("confirm"):
+            return("must provide password confirmation")
+        elif not request.form.get("email"):
+            return("must provide email")
+        
+        rows = get_user(username)
+        if not rows and password == confirm:
+            insert_user(username, email, generate_password_hash(password))
+            session["user_id"] = id
+            print("id")
+            print(session["user_id"])
+            return redirect("/")
+        return "post"
+    else:
+        return render_template("signup.html")
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine, text
 import os
+from functools import wraps
+from flask import session, redirect
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,6 +15,33 @@ engine = create_engine(
     }
     )
 
-with engine.connect() as conn:
-    result = conn.execute(text("Select * from user"))
-    print(result.all())
+def get_user(username):
+    with engine.connect() as conn:
+        result = conn.execute(text(f"select * from user where username = '{username}'"))
+        column_names = result.keys()
+
+        result_dicts = []
+
+        for row in result.all():
+            result_dicts.append(dict(zip(column_names, row)))
+
+        print(result_dicts)
+        return(result_dicts)
+    
+def insert_user(username, email, password_hash):
+    with engine.connect() as conn:
+        conn.execute(text(f"INSERT into user (username, email, password_hash) values ('{username}', '{email}', '{password_hash}')"))
+
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
